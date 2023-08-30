ab76622494090466f57e9ef092d9adfc07b5cac7
VSNSRUJANA123 <srujanadvsn@gmail.com>
2023-08-30 17:20:48 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div>
       working
       <h1> I changed some in mybranch and check it is merged in main</h1>
+      <h2>now i delete the branch in github and try to push again</h2>
     </div>
   );
 }
