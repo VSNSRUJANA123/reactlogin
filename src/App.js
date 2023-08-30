76b84b3dc88e4bd3c167496d8a677d1b9446f1b0
VSNSRUJANA123 <srujanadvsn@gmail.com>
2023-08-30 17:00:29 +0530

    changed in mybrach

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div>working</div>;
+  return (
+    <div>
+      working
+      <h1> I changed some in mybranch and check it is merged in main</h1>
+    </div>
+  );
 }
 
 export default App;
